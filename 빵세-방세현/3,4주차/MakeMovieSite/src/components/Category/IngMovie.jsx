8ce3fae56542bf9b3563157d9c0e6e0c81f27887
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-10-17 22:55:19 +0900

    feat: 미션1_커스텀 훅 카테고리 페이지에 적용

--- 빵세-방세현/3,4주차/MakeMovieSite/src/components/Category/IngMovie.jsx
+++ 빵세-방세현/3,4주차/MakeMovieSite/src/components/Category/IngMovie.jsx
@@ -1,34 +1,29 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Card from "./FrameComponent/Card";
 import MovieList from "./FrameComponent/MovieList";
+import useCustomFetch from "../../hooks/useCustomFetch";
 const IngMovie = () => {
-  const [movies, setMovies] = useState([]);
+  const {
+    data: movies,
+    isLoading,
+    isError,
+  } = useCustomFetch("/movie/now_playing?language=ko&page=1");
 
-  useEffect(() => {
-    // 처음 렌더링 시 데이터를 우선적으로 받아옴
-    const getIngMovie = async () => {
-      const IngMovies = await axios.get(
-        "https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1",
-        {
-          headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2I1MzcwYmRmYmE4NGVhMDIyMWYwYmRhZTU3NjJhYSIsIm5iZiI6MTcyODM2NTIyOC43NzIzMDUsInN1YiI6IjY3MDBmZGUyMTU5MmVmMWJhOTg1NDdkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ikkrqAItlwWIkznFVb7QjERoKQwoLvyYhP7mP4PyKEo`,
-          },
-        }
-      );
-      console.log(IngMovies);
-      setMovies(IngMovies);
-    };
-    getIngMovie();
-  }, []);
-
-  {
-    movies.data?.results.map((movie) => {
-      // 컴포넌트에 데이터 전달
-      <Card key={movie.id} movie={movie} />;
-    });
+  if (isLoading) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
+      </div>
+    );
   }
 
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 중...</h1>
+      </div>
+    );
+  }
   return (
     <MovieList>
       {movies.data?.results.map((movie) => {
--- 빵세-방세현/3,4주차/MakeMovieSite/src/components/Category/PopularMovie.jsx
+++ 빵세-방세현/3,4주차/MakeMovieSite/src/components/Category/PopularMovie.jsx
@@ -1,32 +1,29 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Card from "./FrameComponent/Card";
 import MovieList from "./FrameComponent/MovieList";
+import useCustomFetch from "../../hooks/useCustomFetch";
 
 const PopularMovie = () => {
-  const [movies, setMovies] = useState([]);
+  const {
+    data: movies,
+    isLoading,
+    isError,
+  } = useCustomFetch("/movie/popular?language=ko&page=1");
 
-  useEffect(() => {
-    // 처음 렌더링 시 데이터를 우선적으로 받아옴
-    const getPopularMovie = async () => {
-      const PopularMovies = await axios.get(
-        "https://api.themoviedb.org/3/movie/popular?language=ko&page=1",
-        {
-          headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2I1MzcwYmRmYmE4NGVhMDIyMWYwYmRhZTU3NjJhYSIsIm5iZiI6MTcyODQ1OTc3NC4yMTczNzYsInN1YiI6IjY3MDBmZGUyMTU5MmVmMWJhOTg1NDdkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g4yRsijE8DsrKkeJnk60G5cwyx-6pujHQW4sOO9lUJY`,
-          },
-        }
-      );
-      setMovies(PopularMovies);
-    };
-    getPopularMovie();
-  }, []);
+  if (isLoading) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
+      </div>
+    );
+  }
 
-  {
-    movies.data?.results.map((movie) => {
-      // 컴포넌트에 데이터 전달
-      <Card key={movie.id} movie={movie} />;
-    });
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 중...</h1>
+      </div>
+    );
   }
 
   return (
