c68f2908c775a1bdfb0bb9cae29af24359cb37b3
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-10-19 20:10:47 +0900

    feat: 미션2_영화목록 버튼처리

--- 빵세-방세현/3,4주차/MakeMovieSite/src/components/Category/FrameComponent/Card.jsx
+++ 빵세-방세현/3,4주차/MakeMovieSite/src/components/Category/FrameComponent/Card.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Card = ({ movie }) => {
+  const navigate = useNavigate();
+
+  const movieParticularPage = () => {
+    navigate(`../movie/${movie.id}`, {
+      state: {
+        movieId: movie.id,
+      },
+    });
+  };
+
   return (
-    <Entire>
+    <Entire onClick={movieParticularPage}>
       <EachImage
         src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
         alt="영화 포스터"
@@ -16,28 +27,30 @@ const Card = ({ movie }) => {
 
 export default Card;
 
-const Entire = styled.div`
+const Entire = styled.button`
+  background-color: black;
   display: flex;
   flex-direction: column;
   width: 190px;
+  // justify-content: center;
+  align-items: center;
+  border: 0px;
 `;
 const EachImage = styled.img`
   width: 130px;
   height: 180px;
   border: 1px solid black;
   border-radius: 5px;
-  margin-left: 50px;
+  // margin-left: 50px;
 `;
 
 const MovieTitle = styled.div`
   color: white;
-  margin-left: 50px;
   font-size: 13px;
 `;
 
 const MovieDate = styled.div`
   color: white;
-  margin-left: 50px;
   font-size: 12px;
   margin-bottom: 20px;
 `;
