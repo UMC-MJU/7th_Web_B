746baa77116b4be33500119fab0b5d2bd7416fb4
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-10-27 22:20:06 +0900

    feat:실습1_UI 구현

--- 빵세-방세현/3.4.5주차/MakeMovieSite/src/components/LogInPage.jsx
+++ 빵세-방세현/3.4.5주차/MakeMovieSite/src/components/LogInPage.jsx
@@ -1,13 +1,77 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
 import styled from "styled-components";
 
 const LogInPage = () => {
-  return <LoginMsg>로그인 페이지</LoginMsg>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("폼 데이터 제출");
+    console.log(data);
+  };
+  return (
+    <Screen>
+      <PullForm>
+        <LoginTitle>로그인</LoginTitle>
+        <EmailInput
+          type={"email"}
+          placeholder="이메일을 입력해주세요!"
+          {...register("email")}
+        />
+        <PasswordInput
+          type={"password"}
+          placeholder="비밀번호를 입력해주세요!"
+          {...register("password")}
+        />
+        <SubmitButton type={"submit"} />
+      </PullForm>
+    </Screen>
+  );
 };
 
 export default LogInPage;
 
-const LoginMsg = styled.div`
+const Screen = styled.div`
+  width : 100%
+text-align : center;
+margin : 0 auto;
+`;
+
+const PullForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  height: 500px;
+  justify-content: center;
+  align-items: center;
+`;
+const LoginTitle = styled.h1`
   color: white;
-  margin-left: 500px;
+  margin-bottom: 30px;
+`;
+const EmailInput = styled.input`
+  width: 220px;
+  height: 35px;
+  padding-left: 5px; // placeholder 위치 조정
+  border: 1px solid black;
+  border-radius: 5px;
+  margin-bottom: 20px;
+`;
+const PasswordInput = styled.input`
+  width: 220px;
+  height: 35px;
+  padding-left: 5px; // placeholder 위치 조정
+  border: 1px solid black;
+  border-radius: 5px;
+  margin-bottom: 20px;
+`;
+
+const SubmitButton = styled.input`
+  width: 230px; // padding값 10 추가해서 크기 맞춰줌
+  height: 35px;
+  background-color: rgb(227, 62, 90);
+  color: white;
+  border: 1px solid black;
+  border-radius: 5px;
 `;
