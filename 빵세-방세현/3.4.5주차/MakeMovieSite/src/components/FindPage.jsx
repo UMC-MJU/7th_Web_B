91add77dc5fa02ecf0f0ca7caa7f25b474fafaa0
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-06 17:47:19 +0900

    feat: 미션2_영화 검색 기능 구현(Skeleton UI 전까지)

--- 빵세-방세현/3.4.5주차/MakeMovieSite/src/components/FindPage.jsx
+++ 빵세-방세현/3.4.5주차/MakeMovieSite/src/components/FindPage.jsx
@@ -1,13 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import useCustomFetch from "../hooks/useCustomFetch";
+import MovieList from "./FrameComponent/MovieList";
+import Card from "./FrameComponent/Card";
 
 const FindPage = () => {
-  return <FindMsg>검색페이지</FindMsg>;
+  const [searchValue, setSearchValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // 처음 검색 페이지에 들어왔을 때부터 오류메세지가 뜨는걸 방지
+  const navigate = useNavigate();
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const [searchParams, setSearchParams] = useSearchParams({
+    mq: "",
+  });
+
+  const mq = searchParams.get("mq");
+
+  const handleSearchMovie = () => {
+    if (mq === searchValue) return;
+    navigate(`/find?mq=${searchValue}`); // router에는 /find만 설정해놓았기 때문에 뒤에 어떤게 붙어도 /find까지만 인식
+    setErrorMessage(
+      // 오류문에 변수로 mq를 넣게 되면 오류메세지가 한턴씩 밀려서 나옴.
+      `해당하는 검색어 ${searchValue}에 해당하는 데이터가 없습니다.`
+    );
+    console.log("hi");
+  };
+
+  const handleSearchMovieWithKeyboard = (e) => {
+    if (e.key === "enter") {
+      handleSearchMovie();
+    }
+  };
+
+  /**searchValue값을 넣으면 input 값이 바뀔때마다 화면이 바뀌므로 검색 버튼의 의미가 없어짐
+   * 따라서 mq값을 넣어 검색 버튼을 눌렀을 시 화면이 바뀌도록 하여 UX적으로 나아지도록 함.
+   */
+  const url = `/search/movie?query=${mq}&include_adult=false&language=ko&page=1`;
+  const { data: movies, isLoading, isError } = useCustomFetch(url);
+
+  return (
+    <Screen>
+      <Search>
+        <SearchInput
+          placeholder="영화 제목을 입력해주세요..."
+          value={searchValue}
+          onChange={onChangeSearchValue}
+          onKeyDown={handleSearchMovieWithKeyboard}
+        />
+        <SearchButton onClick={handleSearchMovie}>검색</SearchButton>
+      </Search>
+      {isError || movies?.data?.results.length ? (
+        <MovieList>
+          {movies.data?.results.map((movie) => {
+            return <Card key={movie.id} movie={movie} />;
+          })}
+        </MovieList>
+      ) : (
+        <>
+          <ErrorMessage>{errorMessage}</ErrorMessage>
+        </>
+      )}
+    </Screen>
+  );
 };
 
 export default FindPage;
 
-const FindMsg = styled.div`
+const Screen = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+const Search = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex=1;
+  height: 40px;
+  margin: 20px;
+`;
+const SearchInput = styled.input`
+  height: 30px;
+  flex: 1;
+  border-top-left-raduis: 5px;
+  border-bottom-left-radius: 5px;
+  border: 1px solid rgb(220, 220, 2);
+`;
+
+const SearchButton = styled.button`
+  width: 80px;
   color: white;
-  margin-left: 500px;
+  background-color: rgb(227, 62, 90);
+  cursor: pointer;
+  border: none;
+  border-top-right-raduis: 5px;
+  border-bottom-right-radius: 5px;
+`;
+
+const ErrorMessage = styled.div`
+  color: white;
+  text-align: center;
 `;
