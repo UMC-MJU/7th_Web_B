b53dab4682ceb24f96e8641437995cc64a667727
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-11 21:15:19 +0900

    feat:미션1_카테고리 페이지 useQuery 적용

--- 빵세-방세현/3_7주차/MakeMovieSite/src/components/Category/IngMovie.jsx
+++ 빵세-방세현/3_7주차/MakeMovieSite/src/components/Category/IngMovie.jsx
@@ -1,33 +1,44 @@
 import React from "react";
 import Card from "../FrameComponent/Card";
 import MovieList from "../FrameComponent/MovieList";
-import useCustomFetch from "../../hooks/useCustomFetch";
+import CardSkeleton from "../SkeletonUI/CardSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { useGetMovies } from "../../hooks/queries/useGetMovies";
 const IngMovie = () => {
   const {
     data: movies,
-    isLoading,
+    isPending,
     isError,
-  } = useCustomFetch("/movie/now_playing");
+  } = useQuery({
+    queryFn: () => useGetMovies({ category: "now_playing", pageParam: 1 }), // 매개변수가 있을 때는 화살표 함수 형식으로 작성해주어야 함.
+    queryKey: ["movies", "now_playing"], // 카테고리까지 queryKey값에 넣어주어 다른 페이지에서 받아오는 데이터와 구분시켜줌.
+    cacheTime: 10000, // 10초
+    staleTime: 10000,
+  });
+  // ispending: 데이터를 불러오는 중입니다.
+  // isLoading: 데이터를 불러오는 중이거나, 재시도 중일 때 true가 됨.
 
-  if (isLoading) {
+  if (isPending) {
     return (
-      <div>
-        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
-      </div>
+      <MovieList>
+        {Array.from({ length: 18 }).map((_, index) => (
+          <CardSkeleton key={index} />
+        ))}
+      </MovieList>
     );
   }
 
   if (isError) {
     return (
       <div>
-        <h1 style={{ color: "white" }}>에러 중...</h1>
+        <h1 style={{ color: "white" }}>에러 처리중...</h1>
       </div>
     );
   }
 
   return (
     <MovieList>
-      {movies.data?.results.map((movie) => {
+      {movies?.results?.map((movie) => {
         return <Card key={movie.id} movie={movie} />;
       })}
     </MovieList>
