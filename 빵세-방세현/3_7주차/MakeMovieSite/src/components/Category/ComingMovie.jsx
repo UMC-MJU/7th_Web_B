d4981d0c6ff48b8098177c4b0268cd38b6d05d7c
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-13 16:57:37 +0900

    feat: 미션3_useQuery를 활용한 Pagination 구현

--- 빵세-방세현/3_7주차/MakeMovieSite/src/components/Category/ComingMovie.jsx
+++ 빵세-방세현/3_7주차/MakeMovieSite/src/components/Category/ComingMovie.jsx
@@ -1,74 +1,108 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Card from "../FrameComponent/Card";
 import MovieList from "../FrameComponent/MovieList";
 import CardSkeleton from "../SkeletonUI/CardSkeleton";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getMovies } from "../../hooks/queries/getMovies";
-import { useInView } from "react-intersection-observer";
 const ComingMovie = () => {
+  const [page, setPage] = useState(1);
+
   const {
-    data: movies, // [page, pageParams]가 담김
     isLoading,
     isError,
+    data: movies,
     isFetching,
-    fetchNextPage, // 다음 페이지를 호출하는 함수, getNextPageParam의 return값을 pageParam으로 사용
-    hasNextPage, // getNextPageParam의 리턴값을 통해 다음 페이지가 있는지 판단
-    isFetchingNextPage, // fetchNextPage를 통해 페칭이 되고 있는 상태인지 여부
-  } = useInfiniteQuery({
-    queryKey: ["movies", "upcoming"],
-    queryFn: ({ pageParam = 1 }) =>
-      getMovies({ category: "upcoming", pageParam: pageParam }),
-    getNextPageParam: (lastPage, allPages) => {
-      // lastPage는 마지막으로 fetch된 데이터
-      // allPages는 지금까지 fetch된 모든 페이지의 배열
-      const lastMovie = lastPage.results.at(-1);
-
-      // last무비가 존재하면 다음 페이지 번호로 allPages의 길이 + 1을 반환
-      return lastMovie ? allPages?.length + 1 : undefined;
-    },
+    isPreviousData,
+  } = useQuery({
+    queryKey: ["movies", "upcoming", page],
+    queryFn: () => getMovies({ category: "upcoming", pageParam: page }),
+    keepPreviousData: true,
   });
-
-  const { ref, inView } = useInView({
-    threshold: 0, // 0이면 요소가 1픽셀이라도 뷰포트에 들어오면 true, 1이면 완전히 들어왔을 때만 inview가 true
-  });
-
-  useEffect(() => {
-    if (inView && hasNextPage && !isFetching) {
-      fetchNextPage(); // 바로 함수를 호출하여 실행
-    }
-  }, [inView, hasNextPage, isFetching, fetchNextPage]);
-
-  if (isError) {
-    return (
-      <div>
-        <h1 style={{ color: "white" }}>에러 처리중...</h1>
-      </div>
-    );
-  }
 
   return (
-    <MovieList>
-      {movies?.pages?.map((page) =>
-        page.results.map((movie) => <Card key={movie.id} movie={movie} />)
+    <PullScreen>
+      {isLoading ? (
+        <MovieList>
+          {Array.from({ length: 18 }).map((_, index) => (
+            <CardSkeleton key={index} />
+          ))}
+        </MovieList>
+      ) : isError ? (
+        <div>에러 처리 중...</div>
+      ) : (
+        <MovieList>
+          {movies?.results?.map((page) => (
+            <Card key={page.id} movie={page} />
+          ))}
+        </MovieList>
       )}
-      <CheckView ref={ref}></CheckView>
-      {isLoading ||
-        (isFetchingNextPage && (
-          <MovieList>
-            {Array.from({ length: 18 }).map((_, index) => (
-              <CardSkeleton key={index} />
-            ))}
-          </MovieList>
-        ))}
-    </MovieList>
+      <Buttons>
+        <PreviousButton
+          page={page}
+          onClick={() => setPage((old) => Math.max(old - 1, 0))}
+          disabled={page === 1 || isFetching}
+        >
+          이전
+        </PreviousButton>
+        <PageCount>
+          {page}/{movies?.total_pages}
+        </PageCount>
+        <NextButton
+          page={page}
+          totalPage={movies?.total_pages}
+          onClick={() => {
+            if (!isPreviousData && page < movies?.total_pages) {
+              setPage((old) => old + 1);
+            }
+          }}
+          // Disable the Next Page button until we know a next page is available
+          disabled={
+            isPreviousData || page === movies?.total_pages || isFetching
+          }
+        >
+          다음
+        </NextButton>
+      </Buttons>
+    </PullScreen>
   );
 };
 
 export default ComingMovie;
 
-const CheckView = styled.div`
-  background-color: purple;
-  width: 1px;
-  height: 1px;
+const PullScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const PageCount = styled.div`
+  color: white;
+  text-align: center;
+`;
+
+const Buttons = styled.div`
+  margin-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const PreviousButton = styled.button`
+  color: white;
+  width: 150px;
+  height: 30px;
+  background-color: ${(props) =>
+    props.page === 1 ? "gray" : "rgb(227, 62, 90)"};
+  margin-right: 40px;
+  border: 1px solid black;
+`;
+
+const NextButton = styled.button`
+  color: white;
+  width: 150px;
+  height: 30px;
+  background-color: ${(props) =>
+    props.page === props.totalPage ? "gray" : "rgb(227, 62, 90)"};
+  margin-left: 40px;
+  border: 1px solid black;
 `;
