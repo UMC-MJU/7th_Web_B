92874765bcec57b35382e5da6682430c5d54c230
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-13 13:40:16 +0900

    feat:미션2_영화 카테고리 페이지에 useInfiniteQuery 적용

--- 빵세-방세현/3_7주차/MakeMovieSite/src/components/Category/GoodMovie.jsx
+++ 빵세-방세현/3_7주차/MakeMovieSite/src/components/Category/GoodMovie.jsx
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
 import Card from "../FrameComponent/Card";
 import MovieList from "../FrameComponent/MovieList";
 import CardSkeleton from "../SkeletonUI/CardSkeleton";
-import { useQuery } from "@tanstack/react-query";
-import { useGetMovies } from "../../hooks/queries/useGetMovies";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getMovies } from "../../hooks/queries/getMovies";
+import { useInView } from "react-intersection-observer";
 const GoodMovie = () => {
   const {
-    data: movies,
-    isPending,
+    data: movies, // [page, pageParams]가 담김
+    isLoading,
     isError,
-  } = useQuery({
-    queryFn: () => useGetMovies({ category: "top_rated", pageParam: 1 }), // 매개변수가 있을 때는 화살표 함수 형식으로 작성해주어야 함.
-    queryKey: ["movies", "top_rated"], // 카테고리까지 queryKey값에 넣어주어 다른 페이지에서 받아오는 데이터와 구분시켜줌.
-    cacheTime: 10000, // 10초
-    staleTime: 10000,
-  });
-  // ispending: 데이터를 불러오는 중입니다.
-  // isLoading: 데이터를 불러오는 중이거나, 재시도 중일 때 true가 됨.
+    isFetching,
+    fetchNextPage, // 다음 페이지를 호출하는 함수, getNextPageParam의 return값을 pageParam으로 사용
+    hasNextPage, // getNextPageParam의 리턴값을 통해 다음 페이지가 있는지 판단
+    isFetchingNextPage, // fetchNextPage를 통해 페칭이 되고 있는 상태인지 여부
+  } = useInfiniteQuery({
+    queryKey: ["movies", "top_rated"],
+    queryFn: ({ pageParam = 1 }) =>
+      getMovies({ category: "top_rated", pageParam: pageParam }),
+    getNextPageParam: (lastPage, allPages) => {
+      // lastPage는 마지막으로 fetch된 데이터
+      // allPages는 지금까지 fetch된 모든 페이지의 배열
+      const lastMovie = lastPage.results.at(-1);
 
-  if (isPending) {
-    return (
-      <MovieList>
-        {Array.from({ length: 18 }).map((_, index) => (
-          <CardSkeleton key={index} />
-        ))}
-      </MovieList>
-    );
-  }
+      // last무비가 존재하면 다음 페이지 번호로 allPages의 길이 + 1을 반환
+      return lastMovie ? allPages?.length + 1 : undefined;
+    },
+  });
+
+  const { ref, inView } = useInView({
+    threshold: 0, // 0이면 요소가 1픽셀이라도 뷰포트에 들어오면 true, 1이면 완전히 들어왔을 때만 inview가 true
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching) {
+      fetchNextPage(); // 바로 함수를 호출하여 실행
+    }
+  }, [inView, hasNextPage, isFetching, fetchNextPage]);
 
   if (isError) {
     return (
@@ -38,11 +49,26 @@ const GoodMovie = () => {
 
   return (
     <MovieList>
-      {movies?.results?.map((movie) => {
-        return <Card key={movie.id} movie={movie} />;
-      })}
+      {movies?.pages?.map((page) =>
+        page.results.map((movie) => <Card key={movie.id} movie={movie} />)
+      )}
+      <CheckView ref={ref}></CheckView>
+      {isLoading ||
+        (isFetchingNextPage && (
+          <MovieList>
+            {Array.from({ length: 18 }).map((_, index) => (
+              <CardSkeleton key={index} />
+            ))}
+          </MovieList>
+        ))}
     </MovieList>
   );
 };
 
 export default GoodMovie;
+
+const CheckView = styled.div`
+  background-color: purple;
+  width: 1px;
+  height: 1px;
+`;
