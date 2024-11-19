760437ca1a31e06d8fcd2398d9e906245be9bece
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-19 14:51:25 +0900

    feat:미션1_로딩 및 에러 시 애니메이션 구현

---
+++ 빵세-방세현/8주차/UMCToDoList/src/animation/loadingAni.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const loadingAni = () => {
+  return (
+    <LoadingScreen>
+      <Loader></Loader>
+      {/* <SpinnerImage src={spinnerImage} /> */}
+      <Infor>게시글을 불러오는 중입니다..</Infor>
+    </LoadingScreen>
+  );
+};
+
+export default loadingAni;
+
+const LoadingScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+// Keyframes 정의
+const l51 = keyframes`
+  0% {
+    left: -16px;
+    transform: translateY(-8px);
+  }
+  100% {
+    left: calc(100% + 8px);
+    transform: translateY(22px);
+  }
+`;
+
+const l52 = keyframes`
+  100% {
+    top: -0.1px;
+  }
+`;
+
+const Loader = styled.div`
+  width: 40px;
+  height: 30px;
+  --c: no-repeat linear-gradient(#000 0 0);
+  background: var(--c) 0 100% / 8px 30px, var(--c) 50% 100% / 8px 20px,
+    var(--c) 100% 100% / 8px 10px;
+  position: relative;
+  clip-path: inset(-100% 0);
+  margin-top: 70px;
+  &::before {
+    content: "";
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background: #000;
+    left: -16px;
+    top: 0;
+    animation: ${l51} 2s linear infinite,
+      ${l52} 0.5s cubic-bezier(0, 200, 0.8, 200) infinite;
+  }
+`;
+
+const Infor = styled.div`
+  margin-top: 20px;
+`;
