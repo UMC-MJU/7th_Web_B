b53648e0a4de70f101193d84c58e5d4ddad21d50
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-20 14:42:50 +0900

    feat:미션3_로그인에 useMutation 적용

--- 빵세-방세현/3_7주차/MakeMovieSite/src/components/LogInPage.jsx
+++ 빵세-방세현/3_7주차/MakeMovieSite/src/components/LogInPage.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "../hooks/use-form";
 import styled from "styled-components";
 import { validateLogin } from "../utils/validate";
-import signupInstance from "../apis/signup-instance";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
 const LogInPage = () => {
   const navigate = useNavigate();
 
@@ -15,24 +16,34 @@ const LogInPage = () => {
     validate: validateLogin,
   });
 
-  const handlePressLogin = async (event) => {
-    event.preventDefault(); // 폼 기본 제출 방지
-    try {
-      const response = await signupInstance.post("/auth/login", {
+  const { mutate: PressLogin } = useMutation({
+    mutationFn: async (login) => {
+      const response = await axios.post("http://localhost:3000/auth/login", {
         email: login.values.email, // use-form의 return값들 참고
         password: login.values.password,
       });
+      console.log(response);
+      return response;
+    },
+    onSuccess: (response) => {
+      console.log("로그인 성공", response);
       localStorage.clear();
       localStorage.setItem("id", login.values.email); // 이메일 저장 (회원 이름으로 사용)
       localStorage.setItem("token", response.data.accessToken); // access토큰 저장
       localStorage.setItem("refreshToken", response.data.refreshToken); // refresh토큰 저장
-      console.log(response.data);
       navigate("/movie"); // 영화페이지로 이동
-    } catch (error) {
+    },
+    onError: (error) => {
       console.error("로그인 실패:", error);
       alert("로그인에 실패했습니다. 다시 시도해주세요.");
-    }
+    },
+  });
+
+  const handlePressLogin = (event) => {
+    event.preventDefault(); // 폼 기본 제출 방지
+    PressLogin(login); // useMutation의 mutate 함수를 호출
   };
+
   return (
     <Screen>
       <Form onSubmit={handlePressLogin}>
