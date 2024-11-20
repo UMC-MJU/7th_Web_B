34e9beb6e414e2a1961aa6607092ad3c6363c804
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-20 14:31:57 +0900

    feat:미션3_useMutation을 회원가입에 적용

--- 빵세-방세현/3_7주차/MakeMovieSite/src/components/SignUpPage.jsx
+++ 빵세-방세현/3_7주차/MakeMovieSite/src/components/SignUpPage.jsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import styled from "styled-components";
 import schema from "../schema/SignupSchema";
+import { useMutation } from "@tanstack/react-query";
+
 const SignUpPage = () => {
   const navigate = useNavigate();
 
@@ -29,25 +31,30 @@ const SignUpPage = () => {
       [name]: true,
     });
   };
-  const onSubmit = async (data) => {
-    // 데이터는 는 react-hook-form에서 관리
-    try {
+
+  const { mutate: submitSignUp } = useMutation({
+    mutationFn: async (data) => {
       const response = await axios.post(
         "http://localhost:3000/auth/register",
         data
       );
-      console.log("제출된 데이터", response.data);
+      return response;
+    },
+    onSuccess: (response) => {
+      console.log("회원가입 성공");
+      console.log(response.data);
       navigate("/login");
-    } catch (error) {
+    },
+    onError: (error) => {
       console.error("회원가입 실패:", error);
       alert("회원가입에 실패했습니다. 다시 시도해주세요.");
-    }
-  };
+    },
+  });
 
   return (
     <Screen>
       {/*handleSubmit이 onSubmit의 매개변수 안에 자동으로 폼 데이터를 넣어줌. 또, 유효성 검사 불통과 시 submit 차단*/}
-      <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form onSubmit={handleSubmit(submitSignUp)}>
         <SignupMsg>회원가입 페이지</SignupMsg>;
         <Input
           type="email"
