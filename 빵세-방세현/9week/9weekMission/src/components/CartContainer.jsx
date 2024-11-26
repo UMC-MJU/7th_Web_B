e0465e2ff0f6324b8079b0552bd500da49bffdc0
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2024-11-26 15:23:22 +0900

    feat:미션1_네비바 생성

--- 빵세-방세현/9week/9weekMission/src/components/CartContainer.jsx
+++ 빵세-방세현/9week/9weekMission/src/components/CartContainer.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import styled from "styled-components";
-
+import NavBar from "./NavBar";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 const CartContainer = () => {
   // Redux store에서 cartItems와 total을 가져옵니다.
-  const { cartItems, total } = useSelector((store) => store.cart);
-  console.log(cartItems, total);
+  // 42:57
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  console.log(cartItems);
   return (
     <Section className="cart">
+      <NavBar />
       <Title>당신이 선택한 음반</Title>
       <div>
         {cartItems.map((item) => {
@@ -20,7 +25,12 @@ const CartContainer = () => {
           <h4>
             총 가격 <span>{total}원</span>
           </h4>
-          <button className="btn clear-btn" onClick={() => {}}>
+          <button
+            className="btn clear-btn"
+            onClick={() => {
+              dispatch(clearCart());
+            }}
+          >
             장바구니 초기화
           </button>
         </BottomArea>
@@ -35,10 +45,11 @@ const Section = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100vw;
+  width: 100%;
 `;
 
-const Title = styled.h1`
+const Title = styled.h2`
+  font-size: 40px;
   margin-bottom: 80px;
 `;
 
---
+++ 빵세-방세현/9week/9weekMission/src/components/NavBar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { CartIcon } from "../constants/icons";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+const NavBar = () => {
+  const { amount } = useSelector((state) => state.cart);
+  console.log(amount);
+  return (
+    <Nav>
+      <NavTitle>REAL DATA UMC PlayList</NavTitle>
+      <BagArea>
+        <Bag>
+          <CartIcon />
+        </Bag>
+        <AmountCount>{amount}</AmountCount>
+      </BagArea>
+    </Nav>
+  );
+};
+
+export default NavBar;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  background-color: rgb(114, 114, 188);
+  width: 100%;
+`;
+
+const Bag = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer; // 클릭 가능한 느낌 추가
+  width: 40px; // 아이콘의 크기에 맞게 크기 조정
+  height: 10px;
+`;
+
+const NavTitle = styled.h1`
+  font-size: 50px;
+  color: white;
+  text-align: center;
+`;
+
+const BagArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute; /* Nav 내부에서 자유롭게 배치 가능 */
+  right: 50px; /* 오른쪽 여백 설정 */
+  padding-top: 55px;
+`;
+
+const AmountCount = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+`;
